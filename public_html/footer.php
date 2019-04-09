6ec5fa103537a56bb9ff1b8bdbc883becf88a714
RodianRomeo <Brandon.Fleming@student.nmt.edu>
2019-04-08 22:37:41 -0600

    Silenced errors

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,8 +1,8 @@
 <footer class="footer bg-mute mt-auto">
 
   <div class="text-center">
-          <img src="global_assets/img/logo.png" alt="Logo" height=40px>
-          <img src="global_assets/img/footer_logo.png" alt="Logo" height=40px>
+          <img src="global_assets/img/logo.png" alt="Logo" height="40px"></img>
+          <img src="global_assets/img/footer_logo.png" alt="Logo" height="40px"></img>
   </div>
 
 </footer>
