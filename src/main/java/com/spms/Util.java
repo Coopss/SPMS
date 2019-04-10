debea892bf56a8540f6a377c98e660b68349195c
Coopss <rcooper@nmt.edu>
2019-04-09 19:49:22 -0600

    added dynamic symbol list fetching

--- src/main/java/com/spms/Util.java
+++ src/main/java/com/spms/Util.java
@@ -2,6 +2,8 @@ package com.spms;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
 
@@ -15,5 +17,94 @@ public class Util {
 	public static String removeProtocol(String url) {
 		return url.replaceFirst("^(http[s]?://www\\.|http[s]?://|www\\.)","");
 	}
+	
+	public static String capitalizeFirstLetter(String original) {
+	    if (original == null || original.length() == 0) {
+	        return original;
+	    }
+	    return original.substring(0, 1).toUpperCase() + original.substring(1);
+	}
+	
+	public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
+        List<String> result = new ArrayList<>();
+        if (cvsLine == null && cvsLine.isEmpty()) {
+            return result;
+        }
+
+        if (customQuote == ' ') {
+            customQuote = '"';
+        }
+
+        if (separators == ' ') {
+            separators = ',';
+        }
+
+        StringBuffer curVal = new StringBuffer();
+        boolean inQuotes = false;
+        boolean startCollectChar = false;
+        boolean doubleQuotesInColumn = false;
+
+        char[] chars = cvsLine.toCharArray();
+
+        for (char ch : chars) {
+
+            if (inQuotes) {
+                startCollectChar = true;
+                if (ch == customQuote) {
+                    inQuotes = false;
+                    doubleQuotesInColumn = false;
+                } else {
+
+                    //Fixed : allow "" in custom quote enclosed
+                    if (ch == '\"') {
+                        if (!doubleQuotesInColumn) {
+                            curVal.append(ch);
+                            doubleQuotesInColumn = true;
+                        }
+                    } else {
+                        curVal.append(ch);
+                    }
+
+                }
+            } else {
+                if (ch == customQuote) {
+
+                    inQuotes = true;
+
+                    //Fixed : allow "" in empty quote enclosed
+                    if (chars[0] != '"' && customQuote == '\"') {
+                        curVal.append('"');
+                    }
+
+                    //double quotes in column will hit this!
+                    if (startCollectChar) {
+                        curVal.append('"');
+                    }
+
+                } else if (ch == separators) {
+
+                    result.add(curVal.toString());
+
+                    curVal = new StringBuffer();
+                    startCollectChar = false;
+
+                } else if (ch == '\r') {
+                    //ignore LF characters
+                    continue;
+                } else if (ch == '\n') {
+                    //the end, break!
+                    break;
+                } else {
+                    curVal.append(ch);
+                }
+            }
+
+        }
+
+        result.add(curVal.toString());
+
+        return result;
+    }
+
 	
 }
--- src/main/java/com/spms/ticker/Symbols.java
+++ src/main/java/com/spms/ticker/Symbols.java
@@ -8,14 +8,20 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.spms.Util;
 import com.spms.config.Config;
 import com.spms.database.SPMSDB;
@@ -25,11 +31,10 @@ import com.spms.ticker.tools.Requests;
 public class Symbols {
 
 	private static final Logger log = LogManager.getLogger(Symbols.class);
-	private static HashMap<String, String> testMap = new HashMap<String, String>();
 	private final static String tableName = "internal.los";
-	private static Connection conn;
+	private Connection conn;
 	
-	static {
+	{
 		try {
 			conn = SPMSDB.getConnection();
 		} catch (SQLException e) {
@@ -38,7 +43,7 @@ public class Symbols {
 		}
 	}
 	
-	private static boolean createTable() throws SQLException {				
+	public boolean createTable() throws SQLException {				
 		if (SPMSDB.tableExists(conn, tableName)) {
 			return false;
 		}
@@ -46,7 +51,7 @@ public class Symbols {
 		String makeTableCommand = "CREATE TABLE [dbo].[" + tableName + "]( "
 				+ "[Symbol] [nvarchar](max) NOT NULL, "
 				+ "[Name] [nvarchar](max) NOT NULL,"
-				+ "[MarketCap] [numeric] NOT NULL,"
+				+ "[MarketCap] [nvarchar](max) NOT NULL,"
 				+ "[Sector] [nvarchar](max) NOT NULL,"
 				+ "[Industry] [nvarchar](max) NOT NULL,"
 				+ ") ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]";
@@ -58,48 +63,64 @@ public class Symbols {
 		
 	}
 
-	private static boolean populateTable() {
+	public boolean populateTable() {
 		String csvContent = Requests.followRedirect(Config.endpoint);
 		String lines[] = csvContent.split("\\r?\\n");
+		String header = lines[0];
 		
-		
-		for (String line : lines) {
-			
+		for (String line : Arrays.copyOfRange(lines, 1, lines.length)) {
+			try {
+				insertRow(rowToSymbol(header, line));
+			} catch (SQLException e) {
+				log.error("Failed to insert into " + tableName + " : " + line);
+				log.error(Util.stackTraceToString(e));
+			}
 		}
+	
 		return false;
 		
 	}
 	
-	private static Map<String, String> rowToMap(String header, String row) {
-		HashMap<String, String> map = new HashMap<String, String>();
-		String headerArr[] = header.split(",");		
-		return null;
+	private Boolean insertRow(Symbol sym) throws SQLException {
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate("INSERT INTO [" + tableName + "] (Symbol, Name, MarketCap, Sector, Industry) VALUES ('" + sym.Symbol + "', '" + sym.Name + "', '" + sym.MarketCap + "', '" + sym.Sector + "', '"  + sym.Industry + "');");
+			
+		return true;
 	}
 	
+	private static Symbol rowToSymbol(String header, String row) {
+		Symbol sym = new Symbol();
+		List<String> headerArr = Arrays.asList(header.split(","));	
+		List<String> rowArr = Util.parseLine(row, ',', '"');
+				
+		for (int i = 0 ; i < headerArr.size(); i++) {	
+			switch(headerArr.get(i).trim().replaceAll("\"", "")) {
+			case "Symbol":
+				sym.Symbol = rowArr.get(i);
+				break;
+			case "Name":
+				sym.Name = rowArr.get(i);
+				break;
+			case "MarketCap":
+				sym.MarketCap = rowArr.get(i);
+				break;
+			case "Sector":
+				sym.Sector = rowArr.get(i);
+				break;
+			case "industry":
+				sym.Industry = rowArr.get(i);
+				break;
+			}
+		}
+		return sym;
+	}
 	
 	public static void main(String[] args) throws SQLException, IOException {
-		String csvContent = Requests.followRedirect(Config.endpoint);
-		String lines[] = csvContent.split("\\r?\\n");
-		
+		Symbols s = new Symbols();
 		log.info("Beginning symbol table reload");
-		log.info("Dropping old symbol table: " + SPMSDB.dropTable(conn, tableName));
-		log.info("Creating new symbol table: " + createTable());
-		
-		
-		
-//		String header = lines[0];
-		
-//		System.out.println(header);
-		
-		
-//		for (String line : Arrays.copyOfRange(lines, 1, lines.length)) {
-//			System.out.println(line);
-			
-			
-//		}
-		
-//		System.out.println(csvContent);
-//		testMap.values().stream().map(Object::toString).collect(Collectors.joining(","));
+		log.info("Dropping old symbol table: " + SPMSDB.dropTable(s.conn, tableName));
+		log.info("Creating new symbol table: " + s.createTable());
+		log.info("Populate symbol table from NASDAQ data: " + s.populateTable());		
 	}
 	
 	
