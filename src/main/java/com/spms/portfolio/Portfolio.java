8a219e34e2b774de086ff42baac16f26f3c7dc31
coopss <rcooper@nmt.edu>
2019-04-29 21:55:38 -0600

    Changed expected date format to dd/mm/yyyy on /portfolio/add/{symbol}; fixed constraint checking on same endpoint.

--- src/main/java/com/spms/portfolio/Portfolio.java
+++ src/main/java/com/spms/portfolio/Portfolio.java
@@ -8,8 +8,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.spms.api.PortfolioService;
 import com.spms.ticker.live.TickerDAO;
 import com.spms.ticker.live.TickerData;
+
+import jdk.internal.jline.internal.Log;
 
 public class Portfolio {
 
@@ -17,8 +23,9 @@ public class Portfolio {
 	public Float value = null;
 	public Map<String, Integer> portfolio;
 	private TickerDAO tdao;
-	private List<Transaction> transactions;
+	public List<Transaction> transactions;
 	
+	private static final Logger log = LogManager.getLogger(Portfolio.class);
 	
 	public Portfolio(List<Transaction> transactions, TickerDAO tdao) throws PortfolioConstraintException {
 		this.tdao = tdao;
@@ -26,20 +33,28 @@ public class Portfolio {
 		this.watchlist = new HashSet<String>();
 		this.transactions = transactions;
 		
+		checkValidity();
+	
+	
+		return;
+	}
+	
+	public Boolean checkValidity() throws PortfolioConstraintException {
 		for (Transaction t  : this.transactions) {
 			if (!portfolio.containsKey(t.symbol)) {
 				portfolio.put(t.symbol, t.shares);
 			} else {
 				portfolio.put(t.symbol, portfolio.get(t.symbol) + t.shares);
 			}
-			
-			if (portfolio.get(t.symbol) < 0) {
+		}	
+		
+		for (String sym : this.portfolio.keySet()) {
+			if (portfolio.get(sym) < 0) {
 				throw new PortfolioConstraintException();
 			}
-			
-		}		
-	
-		return;
+		}
+		
+		return true;
 	}
 	
 	
