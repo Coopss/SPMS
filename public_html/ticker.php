dae926453833f69d3628dfe999e471368a032f99
Aptimex <37312852+Aptimex@users.noreply.github.com>
2019-04-30 19:01:20 -0600

    ticker graphs now have a max-height restriction

--- public_html/ticker.php
+++ public_html/ticker.php
@@ -47,17 +47,17 @@
                                                 </span>
                                         </h5>
 
-                                                <div class="chart-area">
+                                                <div class="chart-area" style="max-height:450px">
                                                     <span id="chart_placeholder">Generating graph, please wait...</span>
                                                     <canvas id="myChart" height="400"></canvas>
-
-                                                    <a href="JavaScript:getGraphGranular('1w')">1w</a>
-                                                    <a href="JavaScript:getGraphGranular('1m')">1m</a>
-                                                    <a href="JavaScript:getGraphGranular('3m')">3m</a>
-                                                    <a href="JavaScript:getGraphGranular('1y')">1y</a>
-                                                    <a href="JavaScript:getGraphGranular('5y')">5y</a>
-                                                    <a href="JavaScript:getGraphGranular('max')">max</a>
                                                 </div>
+                                                <a href="JavaScript:getGraphGranular('1w')">1w</a>
+                                                <a href="JavaScript:getGraphGranular('1m')">1m</a>
+                                                <a href="JavaScript:getGraphGranular('3m')">3m</a>
+                                                <a href="JavaScript:getGraphGranular('1y')">1y</a>
+                                                <a href="JavaScript:getGraphGranular('5y')">5y</a>
+                                                <a href="JavaScript:getGraphGranular('max')">max</a>
+                                                
                                                 <div class="card mt-3 tab-card">
                                                         <div class="card-header tab-card-header">
                                                           <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
